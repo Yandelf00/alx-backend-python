8eee741f87f75296792fef44d2dcca1d091865cf
Yandelf00 <zakaria.afyf00@gmail.com>
2024-03-14 20:31:12 +0000

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""task 0"""
+
+
+def add(a: float, b: float) -> float:
+    """add two floats function"""
+    return a + b
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""task 3"""
+
+
+def to_str(n: float) -> str:
+    """task 3"""
+    return str(n)
